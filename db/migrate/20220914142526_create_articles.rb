39f543b66fb920a60c94cbf9f7ed9afff88cab51
Tatiana_Carona <clleidytatina@gmail.com>
2022-09-14 10:23:50 -0500

    MVC finalizado

--- db/migrate/20220914142526_create_articles.rb
+++ db/migrate/20220914142526_create_articles.rb
@@ -4,7 +4,7 @@ class CreateArticles < ActiveRecord::Migration[7.0]
       t.string :title
       t.text :body
 
-      t.timestamps
+      t.timestamps # define dos columnas adicionales created_at u update_at
     end
   end
 end
