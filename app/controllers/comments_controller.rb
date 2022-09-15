ad53ef115b8d79175c2bff6a2cf2b25ad2d6afde
Tatiana_Carona <clleidytatina@gmail.com>
2022-09-15 08:49:47 -0500

    second-model

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,13 @@
+class CommentsController < ApplicationController
+    def create
+      @article = Article.find(params[:article_id]) # busca el articulo asociado
+      @comment = @article.comments.create(comment_params) #crea y guarda el comentario
+      redirect_to article_path(@article) # se envia al usuario de regreso al articulo
+    end
+  
+    private
+      def comment_params
+        params.require(:comment).permit(:commenter, :body,:status)
+      end
+  end
+  
