91722a938ae6fd766fb0c5c0ed7e94837cbb61ce
Tatiana Cardona <clleidytatina@gmail.com>
2022-09-19 09:20:50 -0500

    Code indentation

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,7 @@
 class Article < ApplicationRecord
   include Visible
 
-  has_many :comments, dependent: destroy #elimina los comentarios sí el articulo es eliminado
+  has_many :comments, dependent: :destroy #elimina los comentarios sí el articulo es eliminado
 
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
