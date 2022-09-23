0b61024684ce240952aba97c03588906d5dce38e
Tatiana_Carona <clleidytatina@gmail.com>
2022-09-22 20:18:20 -0500

    Update files

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+
+  #Seguridad para añadir un nuevo articulo
   http_basic_authenticate_with name: "dhh", password: "secret", 
   except: [:index, :show]
 
