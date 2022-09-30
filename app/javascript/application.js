79768b43c8483d122af949816bad121567f2eb02
Tatiana_Carona <clleidytatina@gmail.com>
2022-09-30 13:30:10 -0500

    Mdified devise file

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,8 +1,8 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-import "./popper"
-import "./bootstrap.min"
+import "./popper";
+import "./bootstrap.min"; 
 
 //=require jquery3
 //require popper
