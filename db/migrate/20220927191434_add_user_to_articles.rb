220507c1b781f1f700bf161183d8c2acf152f8e9
Tatiana_Carona <clleidytatina@gmail.com>
2022-09-29 21:36:13 -0500

    Fixed migration to heroku

--- db/migrate/20220927191434_add_user_to_articles.rb
+++
@@ -1,5 +0,0 @@
-class AddUserToArticles < ActiveRecord::Migration[7.0]
-  def change
-    add_reference :articles, :user, null: true, foreign_key: true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -20,8 +20,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_30_001502) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "status"
-    t.bigint "user_id", null: false
-    t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
   create_table "comments", force: :cascade do |t|
@@ -30,9 +28,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_30_001502) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "status"
-    t.bigint "user_id", null: false
     t.index ["article_id"], name: "index_comments_on_article_id"
-    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -45,7 +41,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_30_001502) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "articles", "users"
   add_foreign_key "comments", "articles"
-  add_foreign_key "comments", "users"
 end
